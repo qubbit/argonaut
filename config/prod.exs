fcc836d22d5596d511c27eae69f408b8cea55604
gadhikari <gadhikari@covermymeds.com>
2017-05-06 12:10:49 -0400

    blah

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :argonaut, Argonaut.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "theargonaut-bay.herokuapp.com", port: 80],
+  url: [host: ~s(#{System.get_env("HEROKU_APP_NAME")}.herokuapp.com), port: 80],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
